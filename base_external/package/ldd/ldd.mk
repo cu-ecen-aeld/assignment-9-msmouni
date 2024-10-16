46c25456f24629e43aeab3ff74d7a9ef02427bd5
msmouni <marwan-smouni@hotmail.fr>
2024-10-16 19:37:57 +0200

    Compile `ldd as a module` in Buildroot:
    Without `LDD_MODULE_MAKE_OPTS = KERNELDIR=$(LINUX_DIR)`, the module would not be able to compile because it won't know where the kernel headers are.
    
    Without `$(eval $(kernel-module))`, Buildroot won't treat the package as a kernel module, meaning the module won’t be built and installed properly in the final image.

--- base_external/package/ldd/ldd.mk
+++ base_external/package/ldd/ldd.mk
@@ -10,6 +10,9 @@ LDD_SITE = git@github.com:cu-ecen-aeld/assignment-7-msmouni.git
 LDD_SITE_METHOD = git
 LDD_GIT_SUBMODULES = YES
 LDD_MODULE_SUBDIRS = scull misc-modules
+LDD_MODULE_MAKE_OPTS = KERNELDIR=$(LINUX_DIR)
+
+$(eval $(kernel-module))
 
 define LDD_INSTALL_TARGET_CMDS
 	$(INSTALL) -m 0755 $(@D)/scull/scull_load $(TARGET_DIR)/usr/bin
