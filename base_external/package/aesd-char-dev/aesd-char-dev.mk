3c54383c8a2185276554637de59e832ed8d6df8e
msmouni <marwan-smouni@hotmail.fr>
2024-10-25 13:47:14 +0200

    Update `assignment 3` git version
    + Correct `AESD_CHAR_DEV` module name

--- base_external/package/aesd-char-dev/aesd-char-dev.mk
+++ base_external/package/aesd-char-dev/aesd-char-dev.mk
@@ -5,13 +5,13 @@
 ##############################################################
 
 #TODO: Fill up the contents below in order to reference your assignment 3 git contents
-AESD_ASSIGNMENTS_VERSION = aeb8638e26e41930eb141b5a5109f8b0f6bf1424
+AESD_CHAR_DEV_VERSION = ea41ffe69c4e74f5036eb0eec217d1a72b636bda
 # Note: Be sure to reference the *ssh* repository URL here (not https) to work properly
 # with ssh keys and the automated build/test system.
 # Your site should start with git@github.com:
-AESD_ASSIGNMENTS_SITE = git@github.com:cu-ecen-aeld/assignments-3-and-later-msmouni.git
-AESD_ASSIGNMENTS_SITE_METHOD = git
-AESD_ASSIGNMENTS_GIT_SUBMODULES = YES
+AESD_CHAR_DEV_SITE = git@github.com:cu-ecen-aeld/assignments-3-and-later-msmouni.git
+AESD_CHAR_DEV_SITE_METHOD = git
+AESD_CHAR_DEV_GIT_SUBMODULES = YES
 
 AESD_CHAR_DEV_MODULE_SUBDIRS = aesd-char-driver
 AESD_CHAR_DEV_MODULE_MAKE_OPTS = KERNELDIR=$(LINUX_DIR)
