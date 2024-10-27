af0e3fedd04b2f14f340311ba84e1205236b82d8
msmouni <marwan-smouni@hotmail.fr>
2024-10-27 17:02:32 +0100

    Assignement-9: Update aesd-assignments-3 version

--- base_external/package/aesd-char-dev/aesd-char-dev.mk
+++ base_external/package/aesd-char-dev/aesd-char-dev.mk
@@ -5,7 +5,7 @@
 ##############################################################
 
 #TODO: Fill up the contents below in order to reference your assignment 3 git contents
-AESD_CHAR_DEV_VERSION = ea41ffe69c4e74f5036eb0eec217d1a72b636bda
+AESD_CHAR_DEV_VERSION = 72de59ec849280afc77aa085bf92af38a1e43255
 # Note: Be sure to reference the *ssh* repository URL here (not https) to work properly
 # with ssh keys and the automated build/test system.
 # Your site should start with git@github.com:
